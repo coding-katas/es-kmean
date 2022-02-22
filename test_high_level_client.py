7e394e4ac57824ed8b188751d520c2057be4de2b
Anis TAJOURI <anis.tajouri@gmail.com>
2022-02-22 07:05:26 +0100

    update tests
    Signed-off-by: Anis TAJOURI <anis.tajouri@gmail.com>

--- test_high_level_client.py
+++ test_high_level_client.py
@@ -9,24 +9,24 @@ class TestHighLevelClientSearch(unittest.TestCase):
 
     def test_match_phrase_query_via_low_level_client(self):
         # call the query method
-        search = Search(index='cf_etf', using=ESLowLevelClientByConnection.get_instance()).query(
-            'match_phrase', fund_name='iShares MSCI ACWI ETF')
+        search = Search(index='books', using=ESLowLevelClientByConnection.get_instance()).query(
+            'match_phrase', synopsis='Java Concurrency in Practice')
         response = search.execute()
         self.assertEqual(response['hits']['total']['value'], 1)
         print(response.to_dict())
 
     def test_match_phrase_query_via_connection(self):
         ESLowLevelClientByConnection.get_instance()
-        search = Search(index='cf_etf', using='high_level_client')
+        search = Search(index='books', using='high_level_client')
         # call the Q method
-        search.query = Q('match_phrase', fund_name='iShares MSCI ACWI ETF')
+        search.query = Q('match_phrase', synopsis='Java Concurrency in Practice')
         response = search.execute()
         self.assertEqual(response['hits']['total']['value'], 1)
 
     def test_match_phrase_class_via_connection(self):
         ESLowLevelClientByConnection.get_instance()
         # construct the query object using the class of the query type
-        search = Search(index='cf_etf', using='high_level_client')
-        search.query = MatchPhrase(fund_name='iShares MSCI ACWI ETF')
+        search = Search(index='books', using='high_level_client')
+        search.query = MatchPhrase(synopsis='Java Concurrency in Practice')
         response = search.execute()
         self.assertEqual(response['hits']['total']['value'], 1)
